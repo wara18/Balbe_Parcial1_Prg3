69cb5c4d7309fce41025002261d2915b97dbbb64
Wara <waravilax@email.com>
2026-05-22 23:34:39 -0300

    primer commit, estructura del proyecto

---
+++ src/main/java/Balbe/ParcialPROG3/ParcialProg3Application.java
@@ -0,0 +1,13 @@
+package Balbe.ParcialPROG3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialProg3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialProg3Application.class, args);
+	}
+
+}
---
+++ src/test/java/Balbe/ParcialPROG3/ParcialProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Balbe.ParcialPROG3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialProg3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
