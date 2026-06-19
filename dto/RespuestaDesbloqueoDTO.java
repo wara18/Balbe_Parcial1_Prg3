ccd611c702b77c22612a3cc535647e581bf2e60f
Wara <waravilax@email.com>
2026-06-18 21:00:00 -0300

    completamos losa rchivos dto y cambiamos la clase cargaDatos para agregar strategy, dtos y la busqueda de las estaciones

--- dto/RespuestaDesbloqueoDTO.java
+++ dto/RespuestaDesbloqueoDTO.java
@@ -1,3 +1,16 @@
+package Balbe.ParcialPROG3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// esto es lo que le mandamos al cliente, sin exponer la entidad entera
+@Getter
+@AllArgsConstructor
 public class RespuestaDesbloqueoDTO {
 
+    // solo lo que necesita ver el usuario
+    private String patente;
+    private double costoEstimado;
+    private String estadoVehiculo;
+    private String mensaje;
 }
